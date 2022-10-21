70c67baec00b1ae6a9fd757663d8e76efa8ad853
John Paul Songalia <songaliajohnpaul@gmail.com>
2022-10-21 16:48:27 +0800

    add user home

--- app/src/main/java/com/example/alertosannicolas/AdminsActivity.java
+++ app/src/main/java/com/example/alertosannicolas/AdminsActivity.java
@@ -1,32 +1,65 @@
 package com.example.alertosannicolas;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
+import android.view.Menu;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.alertosannicolas.databinding.ActivityAdminsBinding;
-import com.example.alertosannicolas.databinding.FragmentProfileBinding;
 
 public class AdminsActivity extends AppCompatActivity {
 
-    ActivityAdminsBinding binding;
-    Authentication auth;
+    private AppBarConfiguration mAppBarConfiguration;
+    private ActivityAdminsBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         binding = ActivityAdminsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        auth = new Authentication(this, null);
-
-        binding.signOutBtn.setOnClickListener(new View.OnClickListener() {
+        setSupportActionBar(binding.appBarAdmins.toolbar);
+        binding.appBarAdmins.fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                auth.signOut();
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
+        DrawerLayout drawer = binding.drawerLayout;
+        NavigationView navigationView = binding.navView;
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                .setOpenableLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_admins);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.admins, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_admins);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 }
--- app/src/main/java/com/example/alertosannicolas/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/alertosannicolas/ui/home/HomeFragment.java
@@ -1,10 +1,13 @@
 package com.example.alertosannicolas.ui.home;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
